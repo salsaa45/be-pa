29a559914f45d6eb9a6a5fde6711c607ea08f325
Ghefira Nur Salsabila <161222836+salsaa45@users.noreply.github.com>
2025-06-04 07:36:26 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,12 +6,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'tiket', 
   process.env.DB_USER || 'root', 
-  process.env.DB_PASSWORD || 'lala', {
+  process.env.DB_PASSWORD || '', {
   host: process.env.DB_HOST || 'localhost',
   dialect: 'mysql',
 });
 
 module.exports = sequelize;
-
-
-module.exports = pool;
