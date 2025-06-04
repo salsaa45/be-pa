a53e179df0dfbf6e8af9ee4255913e58a6465b93
Ghefira Nur Salsabila <161222836+salsaa45@users.noreply.github.com>
2025-06-04 07:33:54 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+  process.env.DB_NAME || tiket, 
+  process.env.DB_USER || root, 
+  process.env.DB_PASSWORD || lala, {
+  host: process.env.DB_HOST || localhost,
   dialect: 'mysql',
 });
 
