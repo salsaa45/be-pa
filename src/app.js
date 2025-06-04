fa000f66326dacf4bff3aa2b4f2eee33d22e60ca
Ghefira Nur Salsabila <161222836+salsaa45@users.noreply.github.com>
2025-06-04 07:37:16 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,17 +14,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 
-const sequelize = require('./db');
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Database connected...');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
