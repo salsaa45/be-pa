6482d51b8b6bb808d8adb2b204bd35d252609c1b
Ghefira Nur Salsabila <161222836+salsaa45@users.noreply.github.com>
2025-06-04 07:30:15 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,14 +2,16 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'aisapa',
-  database: process.env.DB_NAME || 'tiket',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 });
+
+module.exports = sequelize;
+
 
 module.exports = pool;
